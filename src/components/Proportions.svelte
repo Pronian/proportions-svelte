7d3993e21069b8bd70702c783c0dd3544f4f4990
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-07 17:15:39 +0200

    Added arrow and improved labels

--- src/components/Proportions.svelte
+++ src/components/Proportions.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
 	import InputNumberExpression from "./InputNumberExpression.svelte";
+	import { arrowNarrowRight } from "../assets/htmlSVGs";
 	import { roundIfNeeded } from "../util/number";
+	const roundingDigits = 3;
 
 	let a = 1;
 	let b = 2;
 	let c = 3;
-	const roundingDigits = 3;
 </script>
 
 
 <div class="flex-cc prop-row">
-	<InputNumberExpression label="Value A" initialValue={a.toString()} on:compute={(event) => a = event.detail} {roundingDigits} />
-	<InputNumberExpression label="Value B" initialValue={b.toString()} on:compute={(event) => b = event.detail} {roundingDigits} />
+		<div class="prop-val">
+			<InputNumberExpression label="value A" initialValue={a.toString()} on:compute={(event) => a = event.detail} {roundingDigits} />
+		</div>
+		<div class="arrow" aria-label="relates to">{@html arrowNarrowRight}</div>
+		<div class="prop-val">
+			<InputNumberExpression label="value B" initialValue={b.toString()} on:compute={(event) => b = event.detail} {roundingDigits} />
+		</div>
 </div>
 <div class="flex-cc prop-row">
-	<InputNumberExpression label="Value C" initialValue={c.toString()} on:compute={(event) => c = event.detail} {roundingDigits}/>
-	<div class="flex-cc prop-res">Result: {roundIfNeeded(c*b/a, roundingDigits)}</div>
+		<div class="prop-val">
+			<InputNumberExpression label="as value C" initialValue={c.toString()} on:compute={(event) => c = event.detail} {roundingDigits}/>
 </div>
+		<div class="arrow" aria-label="to">{@html arrowNarrowRight}</div>
+		<div class="flex-cc prop-val">
+			<div class="prop-res">{roundIfNeeded(c*b/a, roundingDigits)}</div>
+		</div>
+	</div>
 
 <style>
 	.prop-row {
