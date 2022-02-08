a35894ca6861037c8e5cf7f8dfc79a5fcfc17220
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-08 20:32:58 +0200

    Added documentation for `number.ts`

--- src/util/number.ts
+++ src/util/number.ts
@@ -1,4 +1,9 @@
-export function decimalsCount(value: number) {
+/**
+ * Determine the count of decimal places in a number.
+ * @param value - the number to check
+ * @returns - the count of decimal places in the number
+ */
+export function decimalsCount(value: number): number {
   if (!isFinite(value)) return 0;
   let e = 1;
   let decimals = 0;
@@ -9,6 +14,13 @@ export function decimalsCount(value: number) {
   return decimals;
 }
 
+/**
+ * Round a number to a specific number of decimal places only when the number
+ * exceeds the specified number of decimal places.
+ * @param value - the number to round
+ * @param maxPrecision - the maximum number of decimal places to round to
+ * @returns - the rounded number
+ */
 export function roundIfNeeded(value: number, maxPrecision: number): number {
   if (decimalsCount(value) > maxPrecision) {
     return Number(value.toFixed(maxPrecision));
