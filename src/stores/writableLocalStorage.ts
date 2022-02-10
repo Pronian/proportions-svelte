2b978ba0142f87914416f80b9a66e60457f0048a
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-10 18:02:11 +0200

    Added a writable store for localStorage

---
+++ src/stores/writableLocalStorage.ts
@@ -0,0 +1,57 @@
+import { writable } from "svelte/store";
+import { debounce } from "src/util/metaFunctions";
+
+/**
+ * A writable Svelte store that saves its value to localStorage in the background.
+ * @param key - the key to use for the store value in localStorage
+ * @param defaultValue - the default value to use if the store value is
+ * not found in localStorage
+ * @param debounceMs - the number of milliseconds to debounce before saving to
+ * localStorage. Instant saving if not debounced.
+ * @returns a writable Svelte store
+ */
+export function createWritableLS<T>(key: string, defaultValue: T, debounceMs: number = 0) {
+	const { set: storeSet, subscribe } = writable(defaultValue);
+	let storeValue = defaultValue;
+
+	if (localStorage.hasOwnProperty(key)) {
+		try {
+			storeValue = JSON.parse(localStorage.getItem(key) as string);
+		} catch (error) {
+			storeValue = defaultValue;
+		}
+	}
+
+	let setLS: (value: T) => void;
+	if (debounceMs > 0) {
+		setLS = debounce((value: T) => {
+			localStorage.setItem(key, JSON.stringify(value));
+		}, debounceMs);
+	} else {
+		setLS = (value: T) => { localStorage.setItem(key, JSON.stringify(value)); }
+	}
+
+	function wrappedSet(value: T) {
+		storeValue = value;
+		storeSet(value);
+		setLS(value);
+	}
+
+	function update(fn: (value: T) => T) {
+		const updatedValue = fn(storeValue);
+		storeValue = updatedValue;
+		storeSet(updatedValue);
+		setLS(updatedValue);
+	}
+
+	function get() {
+		return storeValue;
+	}
+
+	return {
+		subscribe,
+		set: wrappedSet,
+		update,
+		get
+	};
+}
