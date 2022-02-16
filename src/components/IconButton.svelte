98008fd7bc13d01492a568d580e240c9e334608c
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-16 23:36:55 +0200

    Added IconButton component

---
+++ src/components/IconButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let className = '';
+    export { className as class };
+</script>
+
+<button class={className} {...$$restProps} on:click on:focus on:blur>
+    <slot/>
+</button>
+
+<style>
+    button {
+        aspect-ratio: 1/1;
+        background-color: transparent;
+        color: var(--text-color);
+        border: 0.2rem solid var(--secondary-color-a5);
+        border-radius: 0.8rem;
+        transition: transform, border-color 0.3s ease-in-out;
+    }
+
+    button:hover, button:focus {
+        outline: none;
+        border-color: var(--secondary-color);
+    }
+
+    button:active {
+        transform: scale(0.9);
+    }
+</style>
