f83babf30518107cbf340b252af918690be5b467
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-17 23:28:50 +0200

    Even padding and content centering for the IconButton

--- src/components/IconButton.svelte
+++ src/components/IconButton.svelte
@@ -3,19 +3,19 @@
     export { className as class };
 </script>
 
-<button class={className} {...$$restProps} on:click on:focus on:blur>
+<button class="flex-cc {className}" {...$$restProps} on:click on:focus on:blur>
     <slot/>
 </button>
 
 <style>
     button {
-        display: inline-block;
         aspect-ratio: 1/1;
         background-color: transparent;
         color: var(--text-color);
         outline: none;
         border: none;
         border-radius: 0.8rem;
+        padding: 0.3rem;
         transition: transform, border-color 0.3s ease-in-out;
     }
 
