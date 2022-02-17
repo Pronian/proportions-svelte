7f76da9e5cd9d2a57510f5d77af94d8785c7cdc3
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-02-17 23:29:33 +0200

    Added refresh icon

--- src/assets/svgObjects.ts
+++ src/assets/svgObjects.ts
@@ -19,3 +19,8 @@ export const arrowNarrowRight: SVGObj = {
     attrs: attrsToObj('fill="none" viewBox="0 0 24 24" stroke="currentColor"'),
     body: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>'
 }
+
+export const refresh: SVGObj = {
+    attrs: attrsToObj('fill="none" viewBox="0 0 24 24" stroke="currentColor"'),
+    body: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>'
+}
