955492143c56088e2a29219e37c5d6351508ddb4
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-03-03 20:12:12 +0200

    Manage theme class on body

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,21 @@
 	import Proportions from './components/Proportions.svelte';
 	import Header from './components/Header.svelte';
 	import { generateAlphaColors } from './util/colors';
+	import { settingsStore } from './stores/settingsStore';
 	import { onMount } from 'svelte';
+	let previousTheme: 'dark' | 'light' | undefined;
 
 	onMount(() => {
 		generateAlphaColors(['--bg-color', '--text-color', '--primary-color', '--secondary-color']);
 	});
+
+	$: {
+		if (previousTheme) {
+			document.body.classList.remove(previousTheme);
+		}
+		document.body.classList.add($settingsStore.theme);
+		previousTheme = $settingsStore.theme;
+	}
 </script>
 
 <Header />
