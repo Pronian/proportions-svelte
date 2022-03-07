104a82ec3643533292b79b271fbd10f5823197ad
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-03-07 22:01:22 +0200

    Account for updating settings, removed unneeded body class

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,8 @@
 	import Header from './components/Header.svelte';
 	import { generateAppColors } from './util/colors';
 	import { settingsStore } from './stores/settingsStore';
-	import { onMount } from 'svelte';
-	import type { ColorTheme } from './util/colors';
-	let previousTheme: ColorTheme | undefined;
 
-	onMount(() => {
-		generateAppColors($settingsStore.theme);
-	});
-
-	$: {
-		if (previousTheme) {
-			document.body.classList.remove(previousTheme);
-		}
-		document.body.classList.add($settingsStore.theme);
-		previousTheme = $settingsStore.theme;
-	}
+	$: generateAppColors($settingsStore.theme);
 </script>
 
 <Header />
