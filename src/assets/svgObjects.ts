5a31c4199b6b8d33468074e92f264a8a1884881a
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-03-09 00:15:12 +0200

    Added cog icon, reuse attributes if they don't change

--- src/assets/svgObjects.ts
+++ src/assets/svgObjects.ts
@@ -15,12 +15,19 @@ function attrsToObj(attrs: string): { [key: string]: string } {
 	return obj;
 }
 
+const defaultAttrs = attrsToObj('fill="none" viewBox="0 0 24 24" stroke="currentColor"');
+
 export const arrowNarrowRight: SVGObj = {
-	attrs: attrsToObj('fill="none" viewBox="0 0 24 24" stroke="currentColor"'),
+	attrs: defaultAttrs,
 	body: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>'
 };
 
 export const refresh: SVGObj = {
-	attrs: attrsToObj('fill="none" viewBox="0 0 24 24" stroke="currentColor"'),
+	attrs: defaultAttrs,
 	body: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>'
+};
+
+export const cog: SVGObj = {
+	attrs: defaultAttrs,
+	body: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>'
 };
