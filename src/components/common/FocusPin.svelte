c1ddc41f794e6ef6be27866fdd782d0b4ddb357e
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-03-23 00:05:12 +0200

    Added component to move focus when content is loaded

---
+++ src/components/common/FocusPin.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let div: HTMLDivElement;
+
+	onMount(() => {
+		div.focus();
+	});
+</script>
+
+<div bind:this={div} tabindex="-1" aria-hidden="true"/>
+
+<style>
+	div {
+		opacity: 0;
+		position: absolute;
+		top: -9999px;
+		left: -9999px;
+	}
+</style>
