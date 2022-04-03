394ea750ac0155a9fc124a37787de8f209a34ab9
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-04-03 15:58:04 +0300

    Allow customizing theme colors

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,16 @@
 	import { settingsStore } from './stores/settingsStore';
 	import { uiState } from './stores/uiState';
 
-	$: generateAppColors($settingsStore.theme);
+	$: generateAppColors($settingsStore.theme, $settingsStore.customThemeColors);
+
+	function handleCustomThemeChanged(event: CustomEvent<string[]>) {
+		const colors = event.detail;
+
+		$settingsStore.customThemeColors['bg-color'] = colors[0];
+		$settingsStore.customThemeColors['primary-color'] = colors[1];
+		$settingsStore.customThemeColors['secondary-color'] = colors[2];
+		$settingsStore.customThemeColors['text-color'] = colors[3];
+	}
 </script>
 
 <Header />
@@ -24,7 +33,11 @@
 >
 	<h2>Settings</h2>
 	<FocusPin />
-	<ColorSelector bind:theme={$settingsStore.theme} />
+	<ColorSelector
+		bind:theme={$settingsStore.theme}
+		customColors={Object.values($settingsStore.customThemeColors)}
+		on:customThemeChanged={handleCustomThemeChanged}
+	/>
 	<NumberSelector
 		label="Rounding digits"
 		bind:value={$settingsStore.roundingDigits}
--- src/components/groups/ColorSelector.svelte
+++ src/components/groups/ColorSelector.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import ColorBlock from '../common/ColorBlock.svelte';
 	import { type ColorTheme, lightThemeColors, darkThemeColors } from '../../util/colors';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function customThemeChanged(colors: string[]) {
+		dispatch('customThemeChanged', colors);
+	}
 
 	export let theme: ColorTheme;
+	export let customColors: string[];
 </script>
 
-<div>
+<div class="cont">
 	<ColorBlock
 		class="btn-theme"
 		colors={Object.values(lightThemeColors)}
@@ -20,17 +28,38 @@
 		value="dark"
 		bind:group={theme}
 	/>
+	<ColorBlock
+		class="btn-theme"
+		colors={Object.values(customColors)}
+		label="Custom theme"
+		value="custom"
+		bind:group={theme}
+	/>
+	{#if theme === 'custom'}
+		<div class="customize-cont">
+			Select custom theme colors:
+			{#each customColors as color}
+				<input type="color" bind:value={color} />
+			{/each}
+		</div>
+		<button on:click={() => customThemeChanged(customColors)}>Apply</button>
+	{/if}
 </div>
 
 <style>
-	div {
+	.cont {
 		width: 100%;
 		display: flex;
+		flex-wrap: wrap;
 		padding: 1.5rem;
 		gap: 1.5rem;
 	}
 
-	div :global(.btn-theme) {
-		flex: 1;
+	.cont :global(.btn-theme) {
+		width: calc(50% - 1.5rem / 2);
+	}
+
+	.customize-cont {
+		width: 100%;
 	}
 </style>
