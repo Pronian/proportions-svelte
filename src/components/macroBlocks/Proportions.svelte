1e67ce13b3403ca207e33b61369c9a6e0b508143
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-04-21 22:00:46 +0300

    Refactored layout to use one div per row

--- src/components/macroBlocks/Proportions.svelte
+++ src/components/macroBlocks/Proportions.svelte
@@ -24,7 +24,7 @@
 		Enter values for A, B, which will determine the ratio. And entering value C will use the ratio
 		to generate the new proportion
 	</h2>
-	<div class="flex-cc prop-row">
+	<div class="prop-row">
 		<div class="prop-val" use:swapAnim.actionRotateRight>
 			<InputNumberExpression
 				label="value A"
@@ -42,11 +42,11 @@
 				on:compute={(event) => ($proportionStore.b = event.detail)}
 			/>
 		</div>
+		<IconButton class="btn-action" on:click={swapValues}>
+			<SvgBuilder svgObj={refresh} role="img" title="Invert ratio" />
+		</IconButton>
 	</div>
-	<IconButton on:click={swapValues}>
-		<SvgBuilder svgObj={refresh} role="img" title="Invert ratio" />
-	</IconButton>
-	<div class="flex-cc prop-row">
+	<div class="prop-row">
 		<div class="prop-val">
 			<InputNumberExpression
 				label="as value C"
@@ -56,15 +56,16 @@
 			/>
 		</div>
 		<SvgBuilder class="arrow" svgObj={arrowNarrowRight} role="img" title="to" />
-		<div class="flex-cc prop-val">
+		<div class="prop-val">
 			<div class="prop-res">{result}</div>
 		</div>
+		<div class="btn-action"/>
 	</div>
 </section>
 
 <section class="prop-additional">
 	{#each $proportionStore.cArr as arrC (arrC.id)}
-		<div class="flex-cc prop-row">
+		<div class="prop-row">
 			<div class="prop-val">
 				<InputNumberExpression
 					label="as value C"
@@ -74,13 +75,13 @@
 				/>
 			</div>
 			<SvgBuilder class="arrow" svgObj={arrowNarrowRight} role="img" title="to" />
-			<div class="flex-cc prop-val">
+			<div class="prop-val">
 				<div class="prop-res">{proportionStore.getResult(roundingDigits, arrC.id)}</div>
 			</div>
+			<IconButton class="btn-action" on:click={() => proportionStore.deleteCProp(arrC.id)} >
+				<SvgBuilder class="svg-trash" svgObj={trash} role="img" title="Delete result row" />
+			</IconButton>
 		</div>
-		<IconButton on:click={() => proportionStore.deleteCProp(arrC.id)}>
-			<SvgBuilder class="svg-trash" svgObj={trash} role="img" title="Delete result row" />
-		</IconButton>
 	{/each}
 	<IconButton class="btn-add" on:click={() => proportionStore.addCProp()}>
 		<SvgBuilder class="svg-plus" svgObj={plus} role="img" title="Add result row" />
@@ -91,9 +92,9 @@
 	section {
 		max-width: 768px;
 		width: 100%;
-		display: grid;
-		grid-template: auto / 1fr 3.5rem;
-		gap: 2rem 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 		padding: 0 1rem;
 	}
 
@@ -114,7 +115,8 @@
 	}
 
 	.prop-row {
-		align-items: flex-start;
+		width: 100%;
+		display: flex;
 		gap: 1rem;
 	}
 
@@ -136,12 +138,17 @@
 		height: 3.5rem;
 	}
 
+	.prop-row :global(.btn-action) {
+		width: 3.5rem;
+		height: 3.5rem;
+		margin-bottom: 2rem;
+	}
+
 	.prop-additional {
 		margin-top: 4rem;
 	}
 
 	.prop-additional :global(.btn-add) {
-		grid-column: 1 / span 2;
 		background-color: var(--secondary-color);
 		border-radius: 50%;
 		margin: 0 auto;
