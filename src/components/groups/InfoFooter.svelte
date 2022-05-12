ba48b964d2512d591f4afb8452b6e1c17c5201b1
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-05-12 22:21:41 +0300

    Styled links and improved behavior

--- src/components/groups/InfoFooter.svelte
+++ src/components/groups/InfoFooter.svelte
@@ -3,13 +3,22 @@
 	import { linkedIn, github } from '../../assets/svgObjects';
 </script>
 
-<div>
+<div class="info">
 	<div class="flex-cc links">
-		<a href="https://www.linkedin.com/in/igeorgiev-dev" aria-label="LinkedIn">
-			<SvgBuilder svgObj={linkedIn} role="presentation" />
+		<a href="https://www.linkedin.com/in/igeorgiev-dev" aria-label="LinkedIn" target="_blank">
+			<SvgBuilder svgObj={linkedIn} role="presentation" class="icon"/>
 		</a>
-		<a href="https://github.com/Pronian" aria-label="GitHub">
-			<SvgBuilder svgObj={github} role="presentation" />
+		<a href="https://github.com/Pronian" aria-label="GitHub" target="_blank">
+			<SvgBuilder svgObj={github} role="presentation" class="icon"/>
 		</a>
 	</div>
 </div>
+
+<style>
+	.info :global(.icon) {
+		color: var(--text-color);
+		width: 3.5rem;
+		height: 3.5rem;
+		margin: 1rem;
+	}
+</style>
