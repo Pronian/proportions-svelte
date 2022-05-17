e80b9001b048f1ad472bc686e2194419134491c9
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-05-17 19:35:43 +0300

    Adjusted slide animation so that it can be disabled and so that it can work it static positioning

--- src/transitions/slide.ts
+++ src/transitions/slide.ts
@@ -3,20 +3,38 @@ import { cubicInOut } from 'svelte/easing';
 export function sideSlide(
 	node: HTMLElement,
 	options: {
+		isEnabled?: boolean;
 		duration?: number;
 		delay?: number;
 		easing?: typeof cubicInOut;
 		side?: 'left' | 'right';
+		isAbsolute?: boolean;
 	}
 ) {
-	const { duration = 400, delay = 0, easing = cubicInOut, side = 'left' } = options;
+	const { 
+		isEnabled = true,
+		duration = 400,
+		delay = 0,
+		easing = cubicInOut,
+		side = 'left',
+		isAbsolute = true
+	} = options;
 	const directionModifier = side === 'left' ? -1 : 1;
+
+	if (!isEnabled) {
+		return {};
+	}
+
 	return {
 		delay,
 		duration,
 		easing,
 		css: (_t: number, u: number) => {
-			return `transform: translateX(${node.clientWidth * u * directionModifier}px);`;
+			if (isAbsolute) {
+				return `transform: translateX(${node.clientWidth * u * directionModifier}px);`;
+			} else {
+				return `margin-${side}: -${node.clientWidth * u}px;`;
+			}
 		}
 	};
 }
