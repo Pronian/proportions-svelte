5ab713221ee9d22b6ad6d602014b3d853ed5b927
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-05-22 18:50:21 +0300

    Improved setting elements as inert by not only flagging siblings but also walking up the DOM

--- src/actions/inert.ts
+++ src/actions/inert.ts
@@ -3,31 +3,73 @@ interface Element {
 }
 
 /**
- * Mark the sibling elements of the given element as inert.
+ * Get the siblings of the specified element and update their `inert` attribute.
+ * @param el - the element to get the siblings for
+ * @param enabled - the value to set on the `inert` attribute
+ * @returns the elements whose `inert` attribute was set
+ */
+function setInertForSiblings(el: HTMLElement, enabled: boolean) {
+	const siblings = el.parentElement?.children || [];
+	const modifiedElements: Element[] = [];
+
+	for (const sibling of siblings as Element[]) {
+		if (sibling !== el && 'inert' in sibling) {
+			sibling.inert = enabled;
+			modifiedElements.push(sibling);
+		}
+	}
+
+	return modifiedElements;
+}
+
+/**
+ * Walks up the DOM tree and sets the `inert` attribute on the siblings of the
+ * specified element and the siblings of its parent elements.
+ * @param el - the element from which to start walking up the DOM tree
+ * @param enabled - the value to set on the `inert` attribute
+ * @returns the elements whose `inert` attribute was set
+ */
+function walkUpTreeAndSetInert(el: HTMLElement, enabled: boolean) {
+	let parent = el.parentElement;
+	const modifiedElements: Element[] = setInertForSiblings(el, enabled);
+
+	while (parent && parent.nodeName !== 'BODY') {
+		modifiedElements.push(...setInertForSiblings(parent, enabled));
+		parent = parent.parentElement;
+	}
+
+	return modifiedElements;
+}
+
+/**
+ * Clears the `inert` attribute from the specified elements
+ * by setting it to `false`.
+ * @param elements - the elements whose `inert` attribute should be cleared
+ */
+function clearInertForElements(elements: Element[]) {
+	for (const element of elements) {
+		if ('inert' in element) {
+			element.inert = false;
+		}
+	}
+}
+
+/**
+ * Mark the sibling elements of the given element and the siblings of the
+ * given element's parents as inert.
  * @param el - the element on which the action is applied
  * @param enabled - whether to enable or disable the inert mode
  */
 export function inertSiblings(el: HTMLElement, enabled = false) {
-	let siblings: Element[] | HTMLCollection = [];
-
-	function setInertForSiblings(enabled: boolean) {
-		siblings = el.parentElement?.children || siblings || [];
-
-		for (const sibling of siblings) {
-			if (sibling !== el && 'inert' in sibling) {
-				sibling.inert = enabled;
-			}
-		}
-	}
-
-	setInertForSiblings(enabled);
+	let modifiedElements = walkUpTreeAndSetInert(el, enabled);
 
 	return {
 		update(newEnabled: boolean) {
-			setInertForSiblings(newEnabled);
+			clearInertForElements(modifiedElements);
+			modifiedElements = walkUpTreeAndSetInert(el, newEnabled);
 		},
 		destroy() {
-			setInertForSiblings(false);
+			clearInertForElements(modifiedElements);
 		}
 	};
 }
