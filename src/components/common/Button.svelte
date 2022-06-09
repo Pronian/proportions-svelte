c1bb878e5750f16d30ed54748c72d1be58f6daef
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-06-09 23:41:50 +0300

    Button style improvements

--- src/components/common/Button.svelte
+++ src/components/common/Button.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	let className = '';
 	export { className as class };
-	export const type: 'primary' | 'secondary' | 'transparent' = 'primary';
+	export let type: 'primary' | 'secondary' | 'transparent' = 'primary';
 </script>
 
 <button class="flex-cc {type} {className}" {...$$restProps} on:click on:focus on:blur>
@@ -15,8 +15,10 @@
 		font-size: 1.8rem;
 		outline: none;
 		border: none;
-		border-radius: 2rem;
+		border-radius: 1rem;
+		margin: 1rem;
 		padding: 1rem;
+		min-width: 8rem;
 		transition: transform, border-color 0.3s ease-in-out;
 	}
 
@@ -34,20 +36,20 @@
 	}
 
 	button.primary {
-		background-color: var(--primary-color);
+		background-color: var(--primary-color-conceal-30);
 	}
 
 	.primary:hover,
 	.primary:focus {
-		background-color: var(--primary-color-conceal-50);
+		background-color: var(--primary-color-conceal-60);
 	}
 
 	button.secondary {
-		background-color: var(--secondary-color);
+		background-color: var(--secondary-color-conceal-30);
 	}
 
 	.secondary:hover,
 	.secondary:focus {
-		background-color: var(--secondary-color-conceal-50);
+		background-color: var(--secondary-color-conceal-60);
 	}
 </style>
